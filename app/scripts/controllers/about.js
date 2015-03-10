217aa769266bfb30bea20e52cb962eb37dd31c2d
Matt Uttridge <matt.uttridge@thomsonreuters.com>
2015-03-10 09:53:45 +0000

    Checkin from yo generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spotifyReferenceAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the spotifyReferenceAppApp
+ */
+angular.module('spotifyReferenceAppApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
